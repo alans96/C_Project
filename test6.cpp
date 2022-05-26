a0caf2dd31a079113cda7aa20abb1dd2f2211d55
Alan Mendes <alasilva.mendes06@gmail.com>
2022-05-26 15:12:50 -0300

    6

--- test6.cpp
+++ test6.cpp
@@ -3,13 +3,20 @@
 using namespace std;
 
 int main(){
-    int x = 10;
+    for (int i = 0; i < 10; i++){ //i++ == i+=1 
+    
+        int x=-10;
+        cout << "Digite um numero: ";
+        cin >> x;
 
-    if (x >= 0){
-        cout << "X eh maior do que zero!\n";
+        if (x > 0){
+            cout << "X eh maior do que zero!\n";
+        }
+        else if (x < 0){
+            cout << "X eh negativo\n";
+        }
+        else {
+            cout << "X eh zero!\n";
+        }
     }
-    else {
-        cout << "X eh menor do que zero!\n";
-    }
-
 }
