ed2f977012275eea7fa62f391a83cfc855209960
Alan Mendes <alasilva.mendes06@gmail.com>
2022-05-26 14:25:57 -0300

    6

---
+++ test6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int x = 10;
+
+    if (x >= 0){
+        cout << "X eh maior do que zero!\n";
+    }
+    else {
+        cout << "X eh menor do que zero!\n";
+    }
+
+}
