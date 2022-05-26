c3dd179c9e9966e059e843611eed7dfb6c39be63
Alan Mendes <alasilva.mendes06@gmail.com>
2022-05-26 19:16:49 -0300

    classe

---
+++ teste8.cpp
@@ -0,0 +1,30 @@
+// Criar uma classe
+
+#include <iostream>
+#include <string>
+
+struct Human{
+    Human(){ // Caso seja necessario que a classe inicie com certos valores
+        name = "Human";
+        age = 0;
+        height = 0.10f;
+        weight = 0.5f;
+    }
+    std::string name;
+
+    int age;
+    float height;
+    float weight;
+};
+
+int main(){
+    Human me;
+
+    me.name = "Alan";
+    me.age = 25;
+    me.height = 1.75f;
+    me.weight = 76.2f;
+    
+
+    system("pause");
+}
