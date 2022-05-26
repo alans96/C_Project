ffc46bc7b397d2654a0c2dfb79b6617f2dc71869
Alan Mendes <alasilva.mendes06@gmail.com>
2022-05-26 14:17:29 -0300

    5

---
+++ test5.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int inteiro = 5; // De -2e9 a 2e9
+    cout << inteiro << endl;
+
+    float real = 5.2; // De 1.2e-38 a 3.4e38
+    cout << real << endl;
+
+    double real2 = 5.2e10; // De 2.2e-308 a 1.79-308
+    cout << real2 << endl;
+
+    bool booleano = true;
+    cout << booleano << endl;
+
+    char letra = 'b';
+    cout << letra << endl;
+
+    string palavra= "bola";
+    cout << palavra << endl;
+
+}
